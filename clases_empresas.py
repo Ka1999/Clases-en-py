6e4d84f0e8704c937dc39d85e362749a4a24f590
Ka1999 <karenvane1024@gmail.com>
2026-07-23 20:09:53 -0500

    Composicion

--- clases_empresas.py
+++ clases_empresas.py
@@ -28,9 +28,33 @@ class Empresa:
         self.empleados = []
     def agregar_empleado(self, empleado):
         self.empleados.append(empleado)
-    def mostrar_libros(self):
+    def mostrar_empleado(self):
         for empleado in self.empleados:
             empleado.show()
+    def buscar_empleado(self):
+        eleccion = input("¿Qué empleado quieres buscar?")
+        encontrado = False
+        for empleado in self.empleados:
+            if empleado.nombre == eleccion:
+                empleado.show()
+                encontrado = True
+                break
+            if not encontrado:
+                print("No existe")
+            
+empleado1 = Empleado(nombre= "Karen Vanessa González Alarcón",
+                     cargo= "Analista de datos PY",
+                     salario=7500000)
+empleado2 = Empleado(nombre= "Andrea Correo",
+                     cargo= "Cantante",
+                     salario=6500000)
 
+empresa = Empresa()
+empresa.agregar_empleado(empleado1)
+empresa.agregar_empleado(empleado2)
+
+empresa.mostrar_empleado()
+
+empresa.buscar_empleado()
 
         
