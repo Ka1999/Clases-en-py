22061012b031354809c6bfd57b4d56901a056936
Ka1999 <karenvane1024@gmail.com>
2026-07-27 19:16:11 -0500

    Plantillas de programación

---
+++ plantillas.py
@@ -0,0 +1,26 @@
+#Buscar
+encontrado = False
+
+for elemento in self.lista:
+    if ...:
+        ...
+        encontrado = True
+        break
+
+if not encontrado:
+    print(...)
+
+# Mostrar
+for elemento in self.lista:
+    elemento.show()
+
+# Agregar
+self.lista.append(elemento)
+
+#Actualizar
+for elemento in self.lista:
+    if ...:
+        elemento.atributo = nuevo_valor
+
+#Eliminar
+self.producto.remove(producto)
